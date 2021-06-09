2e7715cf56cbc3a4004bfa9715c34603003e16fe
Gus Narea <gnarea@users.noreply.github.com>
2021-06-09 14:26:11 +0100

    feat: Implement key stores (#2)

---
+++ src/lib/entities/PrivateKey.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+import { PrivateKeyType } from './PrivateKeyType';
+
+@Entity()
+export class PrivateKey {
+  @PrimaryColumn()
+  public readonly id!: string;
+
+  @Column()
+  public readonly derSerialization!: Buffer;
+
+  @Column({ type: 'simple-enum', enum: PrivateKeyType })
+  public readonly type!: PrivateKeyType;
+
+  @CreateDateColumn()
+  public readonly creationDate!: Date;
+
+  @Column({ type: 'blob', nullable: true })
+  public readonly certificateDer!: Buffer | null;
+
+  @Column({ type: 'varchar', nullable: true })
+  public readonly recipientPublicKeyDigest!: string | null;
+}
