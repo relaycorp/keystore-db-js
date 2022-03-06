45a2dcbd26bd3812e096b093307d40d33c118304
Gus Narea <gnarea@users.noreply.github.com>
2022-03-06 15:55:56 +0100

    feat: Export all entities in a single array (`ENTITIES`)
    To make it easier to configure this lib.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-export { Certificate } from './lib/entities/Certificate';
-export { PrivateKey } from './lib/entities/PrivateKey';
-export { PublicKey } from './lib/entities/PublicKey';
+import { Certificate } from './lib/entities/Certificate';
+import { PrivateKey } from './lib/entities/PrivateKey';
+import { PublicKey } from './lib/entities/PublicKey';
 
 export { DBCertificateStore } from './lib/DBCertificateStore';
 export { DBPrivateKeyStore } from './lib/DBPrivateKeyStore';
 export { DBPublicKeyStore } from './lib/DBPublicKeyStore';
+
+export { Certificate, PrivateKey, PublicKey };
+// noinspection JSUnusedGlobalSymbols
+export const ENTITIES: ReadonlyArray<new () => any> = [Certificate, PrivateKey, PublicKey];
